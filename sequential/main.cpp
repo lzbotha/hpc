ee3dbba37664e4f809021171b62af6757bfcf03e
Leonard Botha <bzl@dell-xps>
2015-07-04 17:51:55 +0200

    adapted main file slightly to run a specific median filter

--- sequential/main.cpp
+++ sequential/main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 
 #include "grid.h"
 #include "utils.h"
 
 int main(int argc, char *argv[]) {
     using namespace std;
-    // std::cout << argc << std::endl;
-    // if (argc > 1)
-    //     for (int i = 0; i < argc; ++i)
-    //         std::cout << argv[i] << std::endl;
-    // int bytes = utils::getFileSize("../data/Points_[2.0e+08]_Noise_[030]_Normal.bin");
-    // float * arr = utils::readFile("../data/Points_[2.0e+08]_Noise_[030]_Normal.bin");
 
-    // for (int i = 0; i < 10; ++i)
-    //     cout << arr[i][0] << "\t" << arr[i][1] << endl;
+    clock_t start;
+    start = std::clock();
 
     Grid g(4096, 4096);
+    cout << "Creating grid object: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << endl;
+    start = std::clock();
+
     g.populateFromFile("../data/Points_[2.0e+08]_Noise_[030]_Normal.bin");
+    cout << "Reading file into grid: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << endl;
+    start = std::clock();
+
+    g.applyMedianFilter(3);
+    cout << "Applying median filter: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << endl;
+    start = std::clock();
+    
+    g.printToFile("Bins_[2.0e+08].csv");
+    cout << "Printing to file: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << endl;
     // g.print();
     return 0;
 }
